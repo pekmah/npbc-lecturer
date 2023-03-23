4fca0b35808ee38337f399d319d1bc3241d2e399
Pekmah <ericpekmah@gmail.com>
2023-03-23 22:26:33 +0300

    feat(NPBC-1): created theme, base variables and folder structure

---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export * from "./layout/index"
+export * from "./general/index"
---
+++ src/constants/Theme.js
@@ -0,0 +1,28 @@
+ const colors = {
+    c_blue: "#092A59",
+    c_blue_dark: "#061E40",
+    c_red: "#A22531",
+    c_red_pale: "#A2253121",
+    c_red_glassy: "#A22531E8",
+    c_black: "#000",
+    c_white: "#fff",
+    c_gray:"#7D7C7C",
+    line_gray:"#D9D9D9",
+}
+
+ const txt_sizes = {
+    normal: 14,
+    medium: 16,
+    large: 18,
+    xl: 20,
+    "2xl": 26,
+    "3xl": 28,
+    "4xl": 29,
+    "5xl": 31,
+    "6xl": 32,
+    "7xl": 36,
+    "8xl": 46,
+}
+
+const Theme = { colors, txt_sizes }
+export default Theme
