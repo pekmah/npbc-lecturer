a2f63c16454f1c4ced242f2ddeb714c86a3111ee
Pekmah <ericpekmah@gmail.com>
2023-03-24 11:35:41 +0300

    refractor(NPBC): navbar height h-14 -> h-16

--- src/components/home/Navbar.js
+++ src/components/home/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = () => {
     const [currentTab, setCurrentTab] = useState("home")
 
     return (
-        <div className='bg-c-blue h-14 px-10 flex justify-between items-center '>
+        <div className='bg-c-blue h-16 px-10 flex justify-between items-center '>
             {/* Nav Items */}
             {nav_list?.map((item) => (
                 <NavItem title={item?.name} isCurrent={currentTab?.toLocaleLowerCase() === item?.name?.toLocaleLowerCase()} key={item?.name?.toLocaleLowerCase()} handleClick={() => setCurrentTab(item?.name?.toLocaleLowerCase())} />
