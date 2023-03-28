3622f27ad61b31b5c41035f78528bf10e1efa359
Pekmah <pekstaar@gmail.com>
2023-03-28 14:13:57 +0300

    chore: moved head to layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,10 @@
 import { AboutUs, Admission, Carousel, News, Questions } from '@/components'
 import Layout from '@/layout'
-import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Nairobi Pentecostal Bible College</title>
-        <meta name="description" content="Nurturing potential, building character" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/images/layout/logo.png" />
-      </Head>
+
 
       <Layout>
 
