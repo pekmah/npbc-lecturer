197ad4feb0bf8a72b96141e5bf20dce7d483e46e
Pekmah <ericpekmah@gmail.com>
2023-03-30 22:23:10 +0300

    fix: Carousel image height fixed(60 -> 70)

--- src/components/home/carousel/index.js
+++ src/components/home/carousel/index.js
@@ -12,18 +12,18 @@ const Carousel = ({
         <div className="block relative">
             <div className="h-[70vh] relative">
                 <Car autoPlay showArrows={false} showIndicators={false} showThumbs={false} showStatus={false} infiniteLoop dynamicHeight={"90vh"}>
-                    <div className='relative h-[60vh]'>
-                        <img className='h-[60vh] object-cover' src="/images/home/carousel-1.jpg" alt='img-1' />
+                    <div className='relative h-[70vh]'>
+                        <img className='h-[70vh] object-cover' src="/images/home/carousel-1.jpg" alt='img-1' />
 
                     </div>
 
-                    <div className='relative h-[60vh]'>
-                        <img className='h-[60vh] object-cover' src="/images/home/carousel-2.jpg" alt='img-2' />
+                    <div className='relative h-[70vh]'>
+                        <img className='h-[70vh] object-cover' src="/images/home/carousel-2.jpg" alt='img-2' />
 
                     </div>
 
-                    <div className='relative h-[60vh]'>
-                        <img className='h-[60vh] object-cover' src="/images/home/carousel-3.jpg" alt='img-3' />
+                    <div className='relative h-[70vh]'>
+                        <img className='h-[70vh] object-cover' src="/images/home/carousel-3.jpg" alt='img-3' />
                     </div>
                 </Car>
 
