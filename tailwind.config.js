d3c5d1778eabde0055ef894d9019876caab1cd88
Pekmah <ericpekmah@gmail.com>
2023-03-31 06:28:47 +0300

    chore(NPBC): bg-blue color added to tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        'c-blue': "#092A59",
-        'c-blue-dark': "#061E40",
-        'c-red': "#A22531",
-        'c-red-pale': "#A2253121",
-        'c-red-glassy': "#A22531E8",
-        'c-black': "#000000",
-        'c-white': "#ffffff",
-        'c-gray': "#7D7C7C",
-        'line-gray': "#D9D9D9",
-      },
+        // Or if using `src` directory:
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'c-blue': "#092A59",
+                'c-blue-dark': "#061E40",
+                'c-red': "#A22531",
+                'c-red-pale': "#A2253121",
+                'c-red-glassy': "#A22531E8",
+                'c-black': "#000000",
+                'c-white': "#ffffff",
+                'c-gray': "#7D7C7C",
+                'line-gray': "#D9D9D9",
+                'bg-blue': "#092A5914"
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
