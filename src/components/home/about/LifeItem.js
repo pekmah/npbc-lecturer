a7a6d010834388829ce2cb307bfb647daf565b2d
Asahel Enock Kigen <kirwaasahel@gmail.com>
2023-04-10 10:59:34 +0300

    [UPDATE]: Addition to animation to home page

--- src/components/home/about/LifeItem.js
+++ src/components/home/about/LifeItem.js
@@ -4,9 +4,9 @@ const LifeItem = ({ style, title, children, desc }) => {
     return (
         <div className='relative h-[400px] w-1/4' style={style}>
             <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-col items-center justify-center text-center text-white px-2">
-                <span className="c-h7 text-2xl leading-10">{title}</span>
+                <span className="c-h7 text-2xl leading-10" data-aos="fade-up">{title}</span>
 
-                <p className="mt-4">
+                <p className="mt-4" data-aos="fade-up">
                     {desc}
                 </p>
             </div>
--- src/components/home/carousel/CourseItem.js
+++ src/components/home/carousel/CourseItem.js
@@ -4,7 +4,7 @@ const CourseItem = ({ style, title, children }) => {
     return (
         <div className='relative h-[350px] w-1/4' style={style}>
             <div className="absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center text-center">
-                <span className="c-h6 text-2xl leading-10 text-white">{title || children}</span>
+                <span className="c-h6 text-2xl leading-10 text-white" data-aos="fade-up">{title || children}</span>
             </div>
         </div>
     )
