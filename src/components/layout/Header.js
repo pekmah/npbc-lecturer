ca94f3ef569bcc2317e97d69ad77d575e937465b
pekstar <pekstaar@gmail.com>
2023-05-05 07:09:06 +0300

    feat: responsive Nav  bar

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -6,14 +6,13 @@ import {MdPersonOutline} from 'react-icons/md'
 const Header = ({showModal}) => {
     return (
         <div className='bg-white md:px-10 px-3 h-[75px] flex items-center text-black'>
-            <div className="flex flex-col md:flex-col gap-[0.5] md:gap-4 justify-center md:items-center md:flex-1">
+            <div className="flex flex-col md:flex-row gap-[0.5] md:gap-4 justify-center md:items-center ">
                 {/* Image */}
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img className=' h-10 md:h-[80px] w-[70px] object-contain' src="/images/layout/npbc-logo-2.png" alt="logo" />
+                <img className=' h-12 md:h-[80px] w-[70px] object-contain' src="/images/layout/npbc-logo-2.png" alt="logo" />
 
                 {/* title */}
                 <h4 className="c-h7 text-xl font-bold text-c-blue hidden md:inline-flex">Nairobi Pentecostal Bible College</h4>
-                <h4 className="c-h7 c-large leading-6 font-bold text-c-blue inline-flex md:hidden text-center mx-auto ">NPBC</h4>
             </div>
 
             {/* buttons */}
