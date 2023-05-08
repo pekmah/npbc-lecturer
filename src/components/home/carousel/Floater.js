36ec435dfed6d20529a3318b678596c3bb97c1d7
pekstar <pekstaar@gmail.com>
2023-05-08 15:02:57 +0300

    fix: hidden floater

--- src/components/home/carousel/Floater.js
+++ src/components/home/carousel/Floater.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Floater = ({ title, subTitle }) => {
     return (
         <div className="w-full absolute top-0 bottom-0 right-0 left-0 z-20 flex" data-aos="fade-up">
-            <div className=" mt-[97%] md:mt-[10%] px-4 md:px-10 flex-1">
+            <div className=" mt-[93%] md:mt-[10%] px-4 md:px-10 flex-1">
                 <div className="w-52 bg-c-red py-3 font-bold text-center">
                     <span className="font-bold text-white">
                         {title}
