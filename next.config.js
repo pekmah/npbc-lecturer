8b228f28a6288af1b4e453b4882382a78e88c323
Eric Maina <ericpekmah@gmail.com>
2024-02-20 06:49:22 +0300

    config update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
+module.exports = nextConfig;
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const path = require("path");
+
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
