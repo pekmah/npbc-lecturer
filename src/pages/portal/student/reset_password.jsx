9ae206ea693fb2a40b139bb4b1d566e95cd95c03
pekmah <51937220+pekmah@users.noreply.github.com>
2024-03-02 12:56:12 +0300

    feat: reset password form added

--- src/pages/portal/student/reset_password.jsx
+++ src/pages/portal/student/reset_password.jsx
@@ -10,12 +10,10 @@ const ResetPassword = () => {
       <h4 className={"text-xl md:text-2xl font-semibold text-c-blue "}>
         Reset your password
       </h4>
-
       <p className={"text-c-blue text-sm my-1 mb-3"}>
         To reset your password, enter your registration number for
         identification.
       </p>
-
       <LabelledInput
         required={true}
         labelClassName={"px-1"}
@@ -24,10 +22,9 @@ const ResetPassword = () => {
         inputClassName={"rounded-xl h-12"}
         placeholder={"Enter your registration number"}
       />
-
       <div className={"w-full h-24"} />
       <Button type={"submit"} className={"bg-c-red text-white rounded-xl h-12"}>
-        Login
+        Proceed
       </Button>
     </FormLayout>
   );
