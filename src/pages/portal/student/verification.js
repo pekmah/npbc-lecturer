d0a44cf504870a78fb5fd7dd62dbbf8c4f6a11a7
pekmah <51937220+pekmah@users.noreply.github.com>
2024-03-04 09:12:17 +0300

    feat: verification input added

--- src/pages/portal/student/verification.js
+++ src/pages/portal/student/verification.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FormLayout } from "@/pages/portal/student/_components";
-import { LabelledInput } from "@/components";
 import { Button } from "@/components/ui/button";
+import { VerificationCodeInput } from "@/components/general/LabelledInput";
 
 const ResetPasswordVerification = () => {
   return (
@@ -9,20 +9,21 @@ const ResetPasswordVerification = () => {
       <h4 className={"text-xl md:text-2xl font-semibold text-c-blue "}>
         Reset your password
       </h4>
-      <p className={"text-c-blue font-light text-sm my-4"}>
+      <p className={"text-c-blue font-light text-sm my-4 lg:w-3/4"}>
         We sent a 5 digit verification code to your primary email
         n****@gmail.com.
       </p>
 
-      <LabelledInput
+      <VerificationCodeInput
         required={true}
         labelClassName={"px-1"}
         containerClassName={"gap-2.5"}
-        title={"Registration Number"}
-        inputClassName={"rounded-xl h-12"}
-        placeholder={"Enter your registration number"}
+        title={"Verification Code"}
+        inputClassName={"rounded-xl h-14 w-14"}
       />
+
       <div className={"w-full h-24"} />
+
       <Button type={"submit"} className={"bg-c-red text-white rounded-xl h-12"}>
         Confirm
       </Button>
