7a296268c130f14fb59eae1aa738090ee6cf4019
pekmah <51937220+pekmah@users.noreply.github.com>
2024-03-06 17:33:19 +0300

    feat(#26): side nav icons added

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,3 +1,15 @@
 import NotificationBellSvg from "./NotificationBell";
+import BookSvg from "./Book";
+import ProfileSvg from "./Profile";
+import ReceiptSvg from "./Receipt";
+import WalletSvg from "./Wallet";
+import DashboardSvg from "./Dashboard";
 
-export { NotificationBellSvg };
+export {
+  NotificationBellSvg,
+  BookSvg,
+  ProfileSvg,
+  WalletSvg,
+  ReceiptSvg,
+  DashboardSvg,
+};
