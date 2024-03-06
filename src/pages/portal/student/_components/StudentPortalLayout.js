6976ca510263db14f7029cc701b490b37ee4c994
pekmah <51937220+pekmah@users.noreply.github.com>
2024-03-06 17:55:04 +0300

    feat(#26): side nav added

--- src/pages/portal/student/_components/StudentPortalLayout.js
+++ src/pages/portal/student/_components/StudentPortalLayout.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Navbar from "./nav";
+import SideNav from "./sidenav";
 
 const StudentPortalLayout = ({ children }) => {
   return (
     <div>
       {/*navbar */}
       <Navbar />
-      {children}
+
+      <div className={"flex"}>
+        {/*side nav */}
+        <SideNav />
+
+        <div className={"w-full h-full p-5"}>{children}</div>
+      </div>
     </div>
   );
 };
