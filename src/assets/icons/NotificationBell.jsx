063aa575b3cefc70ceafbae73407dfb6ab05f96a
pekmah <51937220+pekmah@users.noreply.github.com>
2024-03-06 16:50:50 +0300

    feat(#25): notification bell icon added

---
+++ src/assets/icons/NotificationBell.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const NotificationBell = () => {
+  return (
+    <svg
+      width="28"
+      height="28"
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M25.8667 19.8667C26.9333 21.8667 28 22.6667 28 22.6667H4C4 22.6667 8 20 8 10.6667C8 6.26669 11.6 2.66669 16 2.66669C16.9333 2.66669 17.7333 2.80002 18.5333 3.06669M13.7333 28C13.9565 28.4059 14.2846 28.7445 14.6833 28.9803C15.082 29.2161 15.5368 29.3405 16 29.3405C16.4632 29.3405 16.918 29.2161 17.3167 28.9803C17.7154 28.7445 18.0435 28.4059 18.2667 28"
+        stroke="#333333"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M24 14.6667C26.2091 14.6667 28 12.8758 28 10.6667C28 8.45755 26.2091 6.66669 24 6.66669C21.7909 6.66669 20 8.45755 20 10.6667C20 12.8758 21.7909 14.6667 24 14.6667Z"
+        stroke="#A22531"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default NotificationBell;
