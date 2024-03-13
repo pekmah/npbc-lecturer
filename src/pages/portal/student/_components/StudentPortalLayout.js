7801b3b964a895074ce513670283c04ca0577d51
pekmah <51937220+pekmah@users.noreply.github.com>
2024-03-13 18:36:24 +0300

    feat(#29): classname added

--- src/pages/portal/student/_components/StudentPortalLayout.js
+++ src/pages/portal/student/_components/StudentPortalLayout.js
@@ -2,17 +2,19 @@ import React from "react";
 import Navbar from "./nav";
 import SideNav from "./sidenav";
 
-const StudentPortalLayout = ({ children }) => {
+const StudentPortalLayout = ({ children, className }) => {
   return (
     <div>
       {/*navbar */}
       <Navbar />
 
-      <div className={"flex"}>
+      <div className={"flex max-h-screen"}>
         {/*side nav */}
         <SideNav />
 
-        <div className={"w-full h-full p-5"}>{children}</div>
+        <div className={`w-full h-full p-5 overflow-y-scroll ${className}`}>
+          {children}
+        </div>
       </div>
     </div>
   );
