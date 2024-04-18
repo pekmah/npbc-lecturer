6c374d00d55c589ce4d9a1eb2fa6ec28c3926a19
pekmah <51937220+pekmah@users.noreply.github.com>
2024-04-18 22:42:05 +0300

    feat: Session provider added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,11 +5,20 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import MainContext from "@/storage/MainContext";
 import Head from "next/head";
+import { SessionProvider } from "next-auth/react";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   useEffect(() => {
     Aos.init({ duration: 1000 });
   }, []);
+
+  const queryClient = new QueryClient();
+
   return (
     <MainContext>
       <Head>
@@ -21,7 +30,12 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/images/layout/npbc-logo-2.png" />
       </Head>
-      <Component {...pageProps} />
+
+      <SessionProvider session={session}>
+        <QueryClientProvider client={queryClient}>
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </SessionProvider>
     </MainContext>
   );
 }
